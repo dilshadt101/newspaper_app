a956876281531487a6c6afa88f07a58f61b370bc
Mohammed Dilshad <dilshadt101@gmail.com>
2021-10-15 09:44:39 +0530

    email field added to signup page

--- users/forms.py
+++ users/forms.py
@@ -8,7 +8,7 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username', 'email', 'age',)
 
 
 class CustomUserChangeForm(UserChangeForm):
